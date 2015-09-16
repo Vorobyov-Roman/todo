ad0636299995bef49c6217922cf17eec098eb707
Vorobyov-Roman <vorobrawr@gmail.com>
2015-09-16 17:41:51 +0300

    moved the check button

--- scripts/todo_item.jsx
+++ scripts/todo_item.jsx
@@ -11,11 +11,12 @@ var TodoItem = React.createClass({
     },
     render: function() {
         return(
-            <a href="#" className={ "list-group-item" + (isTouch ? "" : " my-todo-item") } onClick={ this.onCheck }>
+            <a href="#" className={ "list-group-item" + (isTouch ? "" : " my-todo-item") }>
                 <span className={ this.props.model.status ? "my-todo-checked" : "" }>
                     { this.props.model.text }
                 </span>
-                <UtilButton label="Close" onClick={ this.onRemove }>&times;</UtilButton>
+                <UtilButton label="Close" onClick={ this.onRemove }>&#10008;</UtilButton>
+                <UtilButton label="Close" onClick={ this.onCheck }>&#10004;</UtilButton>
             </a>
         )
     }
