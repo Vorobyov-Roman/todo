f577bdf0508773bae01ef62d4ac209685ae22d53
Vorobyov-Roman <vorobrawr@gmail.com>
2015-10-04 22:27:55 +0300

    proper cookies

--- scripts/index.jsx
+++ scripts/index.jsx
@@ -1,7 +1,9 @@
 import Todo from './model/model';
 import Container from './container';
+import cookies from 'js-cookie';
 
-var data = JSON.parse(document.cookie);
+var data = cookies.get('model');
+data = data ? JSON.parse(data) : null;
 var model = data ? new Todo(data.text, data._state, data.children) : new Todo('Tasks');
 
 React.render(<Container model={ model }></Container>, $('#app')[0]);
